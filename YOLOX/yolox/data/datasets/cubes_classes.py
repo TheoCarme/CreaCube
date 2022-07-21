d28e3e8b96822a33840cefdd7c511796c628a638
TheoCarme <55946370+TheoCarme@users.noreply.github.com>
2022-07-21 13:51:46 +0200

    Add files via upload

---
+++ YOLOX/yolox/data/datasets/cubes_classes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+CUBES_CLASSES = (
+    "white_cube",
+	"red_cube",
+	"blue_cube",
+	"black_cube",
+)
